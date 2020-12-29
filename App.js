813cc31177be972bc071ab613c07ba9d71f13f6e
km-business <76725597+km-business@users.noreply.github.com>
2020-12-29 22:44:02 +0900

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import {createAppContainer} from 'react-navigation';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
+import HomeScreen from './HomeScreen';
+import ChatScreen from './ChatScreen';
+import SettingScreen from './SettingScreen';
+
+const TabNavigator = createBottomTabNavigator({
+  Home: {
+    screen: HomeScreen,
+  },
+  Chat: {
+    screen: ChatScreen,
+  },
+  Setting: {
+    screen: SettingScreen,
+  },
+});
+export default createAppContainer(TabNavigator);
